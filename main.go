62ed2e996c0b0a37690abca4b74d2d6d731fce67
liensanchez <liensilviosanchez@gmail.com>
2023-05-10 16:12:00 -0300

    Bot can be turned On/Off

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello wolrd")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	tokenDiscord := os.Getenv("TOKEN")
+
+	discord, err := discordgo.New("Bot " + tokenDiscord)
+	if err != nil {
+		log.Fatal("Error creating discord sesion")
+	}
+
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("Error opening connection", err)
+	}
+
+	fmt.Println("Bot Runing!")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	discord.Close()
 }
