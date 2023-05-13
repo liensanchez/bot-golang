b727bc4f6dab90da314c4b4cb30c532b8173346f
liensanchez <liensilviosanchez@gmail.com>
2023-05-13 16:00:01 -0300

    modularice

--- main.go
+++ main.go
@@ -18,23 +18,6 @@ type PingCommand struct {
 }
 
 // Register the command with the Discord API
-func registerCommands(s *discordgo.Session) error {
-	guildID := os.Getenv("GUILD_ID")
-
-	commands := []*discordgo.ApplicationCommand{
-		{
-			Name:        "ping",
-			Description: "Responds with Pong!",
-		},
-	}
-
-	_, err := s.ApplicationCommandBulkOverwrite(guildID, "", commands)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
 
 // Handle the slash command
 func handleCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
