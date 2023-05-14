64d3c70632697d1e99470ba87fcfa3eec95c1408
liensanchez <liensilviosanchez@gmail.com>
2023-05-14 19:56:24 -0300

    Bot fixed on/off

--- main.go
+++ main.go
@@ -11,44 +11,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// Define the command structure
-type PingCommand struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-}
-
-// Register the command with the Discord API
-func registerCommands(s *discordgo.Session) error {
-	guildID := os.Getenv("GUILD_ID")
-
-	commands := []*discordgo.ApplicationCommand{
-		{
-			Name:        "ping",
-			Description: "Responds with Pong!",
-		},
-	}
-
-	_, err := s.ApplicationCommandBulkOverwrite(guildID, "", commands)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Handle the slash command
-func handleCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	switch i.ApplicationCommandData().Name {
-	case "ping":
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "Pong!",
-			},
-		})
-	}
-}
-
 func main() {
 
 	//empezamos cargando el .env con nuestras variables
@@ -64,15 +26,6 @@ func main() {
 	if err != nil {
 		log.Fatal("Error creating discord sesion")
 	}
-
-	// Register the slash commands
-	err = registerCommands(discord)
-	if err != nil {
-		log.Fatal("Error registering commands: ", err)
-	}
-
-	// Add the command handler
-	discord.AddHandler(handleCommand)
 
 	err = discord.Open()
 	if err != nil {
